2d6a14e3b6e5402ff78ce92ec450fd21b44755f0
mus_wolf <rustam161723@gmail.com>
2024-04-29 00:25:16 +0300

    add sync

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/React_Crash/",
+  base:"/Crash_App/",
   plugins: [react()],
   server: {
     port: 3000,
